7de5089f7d9a88f371f653bd1cb710988cb06484
Tomasz Adamczyk <tadamczyk@sigma.ug.edu.pl>
2017-10-23 14:35:01 +0200

    Format code

--- src/test/java/com/tomasz/service/FootballTeamServiceTest.java
+++ src/test/java/com/tomasz/service/FootballTeamServiceTest.java
@@ -8,14 +8,25 @@ import com.tomasz.service.FootballTeamService;
 
 public class FootballTeamServiceTest {
 	FootballTeamService footballTeamService = new FootballTeamService();
-	
+
 	private final static String NAME_1 = "FC Liverpool";
 	private final static int YOE_1 = 1892;
 	private final static double VALUE_1 = 512000000.23;
-	
+
 	@Test
-	public void checkConnection(){
+	public void checkConnection() {
 		assertNotNull(footballTeamService.getConnection());
 	}
 
+	@Test
+	public void checkAdding() {
+		FootballTeam footballTeam = new FootballTeam(NAME_1, YOE_1, VALUE_1);
+		footballTeamService.removeAllFootballTeams();
+		assertEquals(1, footballTeamService.addFootballTeam(footballTeam));
+		List<FootballTeam> footballTeams = footballTeamService.getAllFootballTeams();
+		FootballTeam footballTeamRetrieved = footballTeams.get(0);
+		assertEquals(NAME_1, footballTeamRetrieved.getName());
+		assertEquals(YOE_1, footballTeamRetrieved.getYearOfEstablished());
+		assertEquals(VALUE_1, footballTeamRetrieved.getMarketValue(), 0.01);
+	}
 }

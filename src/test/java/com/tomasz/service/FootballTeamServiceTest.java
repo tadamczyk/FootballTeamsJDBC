77b88399005b21d56994cd5ea5291ad29c5c26f8
Tomasz Adamczyk <tadamczyk@sigma.ug.edu.pl>
2017-10-23 11:42:47 +0200

    Update all files

--- src/test/java/com/tomasz/service/FootballTeamServiceTest.java
+++ src/test/java/com/tomasz/service/FootballTeamServiceTest.java
@@ -9,6 +9,10 @@ import com.tomasz.service.FootballTeamService;
 public class FootballTeamServiceTest {
 	FootballTeamService footballTeamService = new FootballTeamService();
 	
+	private final static String NAME_1 = "FC Liverpool";
+	private final static int YOE_1 = 1892;
+	private final static double VALUE_1 = 512000000.23;
+	
 	@Test
 	public void checkConnection(){
 		assertNotNull(footballTeamService.getConnection());

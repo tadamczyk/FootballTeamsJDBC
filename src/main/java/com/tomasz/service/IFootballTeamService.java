3bec543c7f945edbd44a071f847bd0577d1e585b
Tomasz Adamczyk <tadamczyk@sigma.ug.edu.pl>
2017-10-25 11:42:36 +0200

    Change spaces size

--- src/main/java/com/tomasz/service/IFootballTeamService.java
+++ src/main/java/com/tomasz/service/IFootballTeamService.java
@@ -4,28 +4,28 @@ import java.util.List;
 import com.tomasz.domain.FootballTeam;
 
 public interface IFootballTeamService {
-	public int addFootballTeam(FootballTeam footballTeam);
+  public int addFootballTeam(FootballTeam footballTeam);
 
-	public List<FootballTeam> getAllFootballTeams();
+  public List<FootballTeam> getAllFootballTeams();
 
-	public void updateNameFootballTeam(FootballTeam footballTeam, String name);
+  public void updateNameFootballTeam(FootballTeam footballTeam, String name);
 
-	public void updateYearOfEstablishedFootballTeam(FootballTeam footballTeam, int yearOfEstablished);
+  public void updateYearOfEstablishedFootballTeam(FootballTeam footballTeam, int yearOfEstablished);
 
-	public void updateMarketValueFootballTeam(FootballTeam footballTeam, double marketValue);
+  public void updateMarketValueFootballTeam(FootballTeam footballTeam, double marketValue);
 
-	public void removeAllFootballTeams();
+  public void removeAllFootballTeams();
 
-	public void removeFootballTeamById(long id);
+  public void removeFootballTeamById(long id);
 
-	public void removeFootballTeamByName(String name);
+  public void removeFootballTeamByName(String name);
 
-	public FootballTeam findById(long id);
+  public FootballTeam findById(long id);
 
-	public FootballTeam findByName(String name);
+  public FootballTeam findByName(String name);
 
-	public FootballTeam findByYearOfEstablished(int yearOfEstablished);
+  public FootballTeam findByYearOfEstablished(int yearOfEstablished);
 
-	public FootballTeam findByMarketValue(double marketValue);
-	
+  public FootballTeam findByMarketValue(double marketValue);
+
 }
